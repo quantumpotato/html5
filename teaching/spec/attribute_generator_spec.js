c5e9058107983c6f36eb083500cd4acdf8400172
Anthony Broussard <anthony@chaione.com>
2012-03-03 22:30:14 -0600

    Removed Jasmine describe

--- teaching/spec/attribute_generator_spec.js
+++ teaching/spec/attribute_generator_spec.js
@@ -1,28 +1,22 @@
-describe("Jasmine", function() {
-	describe("Basic Object", function(){
-		it("should have 4 properties", function(){
-			expect(PropertyManager.BasicObject.properties.length).toBe(4);
-		});
+describe("Basic Object", function(){
+	it("should have 4 properties", function(){
+		expect(PropertyManager.BasicObject.properties.length).toBe(4);
+	});
+});
+
+describe("Object generator", function(){
+	it("should create an object which has properties from its template", function(){
+		var newThing = generateThing("BasicObject");
+		expect(newThing.properties.length).toBe(PropertyManager["BasicObject"].properties.length);
 	});
 	
-	describe("Object generator", function(){
-		it("should create an object which has properties from its template", function(){
-			var newThing = generateThing("BasicObject");
-			expect(newThing.properties.length).toBe(PropertyManager["BasicObject"].properties.length);
-		});
-		
-		it("should set default values", function(){
-			var newThing = generateThing("BasicObject");
-			var expectedProperties = PropertyManager["BasicObject"].properties;
-			for (var i = 0; i < newThing.properties.length; i++){
-				var propertyName = newThing.properties[i];
-				var propertyValue = newThing[propertyName];
-				expect(propertyValue).toBe(DefaultPropertyManager[propertyName]);
-			};
-		});
+	it("should set default values", function(){
+		var newThing = generateThing("BasicObject");
+		var expectedProperties = PropertyManager["BasicObject"].properties;
+		for (var i = 0; i < newThing.properties.length; i++){
+			var propertyName = newThing.properties[i];
+			var propertyValue = newThing[propertyName];
+			expect(propertyValue).toBe(DefaultPropertyManager[propertyName]);
+		};
 	});
-	
-  it("makes testing JavaScript awesome!", function() {
-//    expect(yourCode).toBe
-  });
 });
