037d18a1200763777270d87f67964a5fdb3cafb7
Anthony Broussard <anthony@chaione.com>
2012-03-09 22:59:32 -0600

    StupidHunter moves when it has a target

--- teaching/spec/stupid_hunter_spec.js
+++ teaching/spec/stupid_hunter_spec.js
@@ -1,5 +1,6 @@
 describe("Stupid Hunter", function(){
 	var stupidHunter;
+	var target = {"l":{"x":10,"y":10}};
 		beforeEach(function(){
 			stupidHunter = generateThing(['BasicObject','StupidHunter']);
 			console.log("new stupid hunter d.x" + stupidHunter.d.x);
@@ -10,9 +11,16 @@ describe("Stupid Hunter", function(){
 		var oldX = stupidHunter.l.x;
 		var oldY = stupidHunter.l.y;
 		stupidHunter.speed = 1;
-	//	alert(stupidHunter.d.x);
 		stupidHunter.functions["tick"][0]({"t":stupidHunter});
 		expect(stupidHunter.l.x).toBe(oldX);
 		expect(stupidHunter.l.y).toBe(oldY);
+	});
+	
+	it("should move when it has a target", function() {
+		stupidHunter.target = target;
+		var oldDistance = distance(stupidHunter.l, stupidHunter.target.l);
+		etf(stupidHunter, "tick", {"t":stupidHunter});
+		etf(stupidHunter, "tick", {"t":stupidHunter});		
+		expect(distance(stupidHunter.l, stupidHunter.target.l)).toBeLessThan(oldDistance);
 	});
 });
