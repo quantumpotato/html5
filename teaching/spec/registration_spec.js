6f3decf7fe757ae277df32c5a3a147e53c963060
Anthony Broussard <anthony@chaione.com>
2012-03-13 17:21:25 -0500

    game controller adds registered objects to its list of nodes

--- teaching/spec/registration_spec.js
+++ teaching/spec/registration_spec.js
@@ -1,15 +1,14 @@
 describe("Registering", function(){
+	var testGC;
 		beforeEach(function(){
-
+			testGC = gameController();
 		});
 		it("should register objects with the game controller", function(){
 			var player = generateThing(["BasicObject","Player"]);
-		alert(gc);
-		alert(gameController());
-	//		alert(foobar);
-//			gameController().register(player);
+			testGC.register(player);
+			expect(testGC.nodes[0]).toBe(player);
 		});
 		it("should pull targets from registered objects", function(){
-
+			
 		});
 });
--- teaching/src/game_controller.js
+++ teaching/src/game_controller.js
@@ -1,14 +1,40 @@
-gameController = function(){
-	this.nodes = [];
-	this.registeredNodes = {};
-	this.eventInterceptors = [];
-	this.initialize = function(){
-		
-	};
-	
-	this.reset = function(){
-		
-	};
-};
+function DefaultPropertyManager() {
+	return {
+		"l":{"x":0,"y":0},
+		"d":{"x":0,"y":0},
+		"life":1,
+		"maxLife":1,
+		"speed":1,
+		"regenRate":1,
+		"mass":1,
+		"score":0
+	}
+}
+
+function gameController() {
+	return {
+		"nodes":[],
+		"registeredNodes":{},
+		"eventInterceptors":[],
+		"initialize":function(){},
+		"reset":function(){},
+		"register":function(t){
+			this.nodes.push(t);
+		}
+	}
+}
+
+// gameController = function(){
+// 	this.nodes = [];
+// 	this.registeredNodes = {};
+// 	this.eventInterceptors = [];
+// 	// this.initialize = function(){
+// 	// 	
+// 	// };
+// 	// 
+// 	// this.reset = function(){
+// 	// 	
+// 	// };
+// };
 
 //registeredObjects[player].length
