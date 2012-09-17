ac3c20bc2136bbde26658f4621eaec6cac8855a9
Anthony Broussard <anthony.broussard@chaione.com>
2012-09-17 16:37:51 -0500

    Progress in functions

--- teaching/games/basicObject.js
+++ teaching/games/basicObject.js
@@ -1,6 +1,13 @@
 function basicObject(){
 var properties = ['l', 'd', 'life', 'maxLife', 'speed', 'power', 'radius'];
-var nt = {'properties':properties};
+var functionIndex = ['tick'];
+var functions = {'tick':[function(args){
+                          var t = args.t;
+                          t.l.x = t.l.x + (t.d.x * t.speed);
+                          t.l.y = t.l.y + (t.d.y * t.speed);
+                        }]};
+
+var nt = {'properties':properties, 'functions':functions, 'functionIndex':functionIndex};
 return nt;
 };
     
--- teaching/src/spawning.js
+++ teaching/src/spawning.js
@@ -1,17 +1,30 @@
 function assignInitialProperty(t, prop) {
- //load from list
-t[prop] = 5; 
 t[prop] = DefaultPropertyManager()[prop];
 };
 
 function assignPropertiesFromComponentTemplate(t, template, completion) {
-     for (var ii = 0; ii < template.properties.length; ii++) {
-        assignInitialProperty(t, template.properties[ii]);       
+     for (var i = 0; i < template.properties.length; i++) {
+        assignInitialProperty(t, template.properties[i]);       
      };
+     if (t.functions === undefined) {
+       t.functions = {};
+     };
+     for (var ii = 0; ii < template.functionIndex.length; ii++) {
+       fName = template.functionIndex[ii];
+       if (t.functions[fName] === undefined) {
+         t.functions[fName] = [];
+       };
+       for (iii = 0; iii < template.functions[fName].length; iii++) {
+         t.functions[fName].push(template.functions[fName][iii]);
+       };
+
+ }
      t.componentsRemainingForAssignment--;
      if (t.componentsRemainingForAssignment == 0) {
        completion(t);
      };
+alert('t.functions[tick]' + t.functions['tick']);
+   
 };
 
 function loadComponents(t, component, completion) {
@@ -29,74 +42,12 @@ function assignComponents(t, components, completion) {
   };
 };
 
-
-
 function spawn(name, gc, completion) {
-
-var t = {};
+  var t = {};
   $.getScript(name + '.js', function(data, b, c) {
     var template = window[name]();
     var components = template.components;
     assignComponents(t, template.components, completion);
   });
-
-}
-
-function spawnThing(name, gc) {
-
-var t = {};
-
-$.getScript(name + '.js', function(data, textStatus, jqxhr) {
-
-var template = window[name]();
-alert('template:' + template);
-var components = template.components; 
-alert('components:' + components);
-alert(components[0]);
-alert('components.length:' + components.length);
-for (var i = 0; i <= components.length; i++ ) {
-//load component file
-alert('loading component file for index:' + i);
-  $.getScript(components[0] + '.js', function(data, textStatus, jqxhr) {
-alert('status'+textStatus);
-  var componentTemplate = window[components[0]]();
- // alert('componentTemplate: ' + componentTemplate);
-  var properties = componentTemplate.properties;
-  for (var ii = 0; ii < properties.length; ii++) {
-    assignInitialProperty(t, properties[ii]);
-    alert('assigned t' + t);
-  }
- alert(t.l);
-alert(t.life);
-
-  });
-//assign properties
-
-//assign functions
-
 };
 
-});
-alert('returning t' + t);
-return t;
-
-};
-
-function st(name, gc){
-return spawnThing(name, gc);
-};
-
-
-
-
-function testSpawn(name, gc) {
-var testedSpawn;
-$.getScript(name + '.js', function(data, textStatus, jqxhr) {
-  testedSpawn = window[name]();
-  alert('test components: ' + testedSpawn.components);
- alert('template:'+testedSpawn);
-});
-
-return testedSpawn;
-
-};
